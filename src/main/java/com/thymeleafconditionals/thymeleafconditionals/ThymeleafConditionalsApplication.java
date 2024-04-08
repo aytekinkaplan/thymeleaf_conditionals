cb6d600cf9214acf919dbdaeb11911e89b925838
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-08 18:43:13 +0300

    Initial commit

---
+++ src/main/java/com/thymeleafconditionals/thymeleafconditionals/ThymeleafConditionalsApplication.java
@@ -0,0 +1,13 @@
+package com.thymeleafconditionals.thymeleafconditionals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafConditionalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafConditionalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thymeleafconditionals/thymeleafconditionals/ThymeleafConditionalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thymeleafconditionals.thymeleafconditionals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafConditionalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
