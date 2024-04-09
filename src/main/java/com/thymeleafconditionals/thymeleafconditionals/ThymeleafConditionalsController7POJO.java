e7c1328b13801e3f4a80bc0d0fcb921f1200decf
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-04-09 11:40:46 +0300

    Commit message

---
+++ src/main/java/com/thymeleafconditionals/thymeleafconditionals/ThymeleafConditionalsController7POJO.java
@@ -0,0 +1,45 @@
+package com.thymeleafconditionals.thymeleafconditionals;
+
+public class ThymeleafConditionalsController7POJO {
+    private String item;
+    private double revenue;
+    private double cost;
+
+    public ThymeleafConditionalsController7POJO(String item, double revenue, double cost) {
+        this.item = item;
+        this.revenue = revenue;
+        this.cost = cost;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public double getRevenue() {
+        return revenue;
+    }
+
+    public void setRevenue(double revenue) {
+        this.revenue = revenue;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public double getProfit() {
+        return calculateProfit();
+    }
+
+    private double calculateProfit() {
+        return revenue - cost;
+    }
+}
